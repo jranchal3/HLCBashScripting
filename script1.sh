54076853d57ab2c4dfaa682f308a9657f93079c0
Juan Ranchal <jranchal3@gmail.com>
2023-10-03 11:12:02 +0200

    Script

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Script: script2
+#Descripción: Mensaje de bienvenida al usuario
+#Autor: Juan Ranchal
+#Fecha: 28/09/2023
+
+clear
+echo "Bienvenido"
+whoami
---
+++ script2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Script: script3
+#Descripción: Muestra el contenido del directorio
+#Autor: Juan Ranchal
+#Fecha: 28/09/2023
+
+clear
+echo "Contenido directorio"
+ls
+date
